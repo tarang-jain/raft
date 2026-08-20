04e4ff1b588e8ffb306a4abcda64b9b327ff3c0a
Joséphine Wolf Oberholtzer <166141926+josephine-wolf-oberholtzer@users.noreply.github.com>
2026-08-20 14:57:21 -0400

    X-ORG-3107: Publish API docs to docs.nvidia.com (#3108)
    Closes #3107
    
    In order to publish docs to docs.nvidia.com, we need to:
    - Integrate a [new shared
    workflow](https://github.com/rapidsai/shared-workflows/blob/main/.github/workflows/publish-api-docs.yaml)
    for pushing to the docs.nvidia.com S3 bucket
    - Activate the version switcher drop-down in Sphinx, using a
    `versions.json` file located on docs.nvidia.com
    
    N.B.:
    - This _does not replace_ publishing docs to docs.rapidsai.org. This is
    _in addition to_.
    - Virtually identical changes have already been made for cudf, cugraph,
    cuvs, nvforest, rmm, etc.
    - Unlike the initial batch of repos, I'm setting the `version-map`
    parameter via `vars` rather than hard-coding. I'll migrate the initial
    batch of repos to use this same pattern in the near future so we can
    coordinate versions independent of code changes.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -121,8 +121,13 @@ html_theme_options = {
             "type": "fontawesome",
         },
     ],
-    "show_toc_level": 1,
     "navbar_align": "right",
+    "navbar_center": "navbar-nav, version-switcher, navbar-external-links",
+    "show_toc_level": 1,
+    "switcher": {
+        "json_url": "https://docs.nvidia.com/raft/versions.json",
+        "version_match": version,
+    },
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
